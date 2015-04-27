76ba50ea837a6e622edaceaace33cbeb8d62ac72
Chris Scott <cscott530@gmail.com>
2015-04-26 23:28:38 -0400

    loading scene

--- SuperKoalio/GameLevelScene.swift
+++ SuperKoalio/GameLevelScene.swift
@@ -8,9 +8,15 @@
 
 import Foundation
 import SpriteKit
+
 class GameLevelScene: SKScene {
+    var map : JSTileMap?
     override init(size: CGSize) {
         super.init(size: size)
+        
+        self.backgroundColor = SKColor(red: 0.4, green: 0.4, blue: 0.95, alpha: 1.0)
+        self.map = JSTileMap(named: "level1.tmx")
+        self.addChild(self.map!)
     }
 
     required init?(coder aDecoder: NSCoder) {
